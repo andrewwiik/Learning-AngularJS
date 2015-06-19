810f673b1fc74787b3a769a00855376f24c574b5
Andrew Wiik <andrew.wiik@vtsd.com>
2015-06-19 16:47:13 -0400

    Update and rename teams.php to contacts.php

--- teams/contacts.php
+++ teams/contacts.php
@@ -1,14 +1,14 @@
 <?php
 //MYSQLI sting to connect to the database
-$mysqli = mysqli_connect("localhost", "bentouss_testing", "b3ntoussi", "bentouss_testing");
+$mysqli = mysqli_connect("localhost", "root", "Sticker101?", "contacts");
 
 switch($_GET["action"]){
 	// Switch case for the team list controller
 	case "list":
-		$query = "SELECT * FROM newteamList";
+		$query = "SELECT * FROM contactList";
 		$result = $mysqli->query($query);
 		while ($row = $result->fetch_assoc()) {
-			$json[] = array('id' => $row['id'], 'name' => $row['name'], 'founded' => $row['founded'], 'city' => $row['city'], 'stadium' => $row['stadium'], 'capacity' => $row['capacity'], 'manager' => $row['manager'], 'websiteLink' => $row['websiteLink'], 'image' => $row['image'], 'details' => $row['details']);
+		$json[] = array('fname' => $row['fname'], 'lname' => $row['lname'], 'address' => $row['address'], 'city' => $row['city'], 'zipcode' => $row['zipcode'], 'mnumber' => $row['mnumber'], 'lnumber' => $row['lnumber'], 'relation' => $row['relation'], 'photo' => $row['photo']);
 		}
 		echo json_encode($json);
 		$mysqli->close();
@@ -16,20 +16,20 @@ switch($_GET["action"]){
 	// Switch case for the team details controller
 	case "detail":
 	 	$id = $_GET['id'];
-		$query = $mysqli->prepare('SELECT * FROM newteamList WHERE id = ?');
+		$query = $mysqli->prepare('SELECT * FROM contactList WHERE id = ?');
 		$query->bind_param('i', $id);
 		$query->execute();
 		$result = $query->get_result();
 		while ($row = $result->fetch_assoc()){
-		 	$json = array('id' => $row['id'], 'name' => $row['name'], 'founded' => $row['founded'], 'city' => $row['city'], 'stadium' => $row['stadium'], 'capacity' => $row['capacity'], 'manager' => $row['manager'], 'websiteLink' => $row['websiteLink'], 'image' => $row['image'], 'details' => $row['details']);
+		 	$json = array('id' => $row['id'], 'fname' => $row['fname'], 'lname' => $row['lname'], 'address' => $row['address'], 'city' => $row['city'], 'zipcode' => $row['zipcode'], 'mnumber' => $row['mnumber'], 'lnumber' => $row['lnumber'], 'relation' => $row['relation'], 'photo' => $row['photo']
 		 }
 		 echo json_encode($json);
 		 $mysqli->close();
 	break;
 	// Switch case for the add team controller
 	case "add":
-		$dataString = $_POST['image'];
-		define('UPLOAD_DIR', '../badges/');
+		$dataString = $_POST['photo'];
+		define('UPLOAD_DIR', '../contactimg/');
 		$img = $dataString;
 		if (preg_match('/png/', $dataString)){
 			$img = str_replace('data:image/png;base64,', '', $img);
@@ -43,18 +43,18 @@ switch($_GET["action"]){
 		$data = base64_decode($img);
 		$success = file_put_contents($file, $data);
 
-		$query = $mysqli->prepare('INSERT INTO newteamList (name, founded, city, stadium, capacity, manager, websiteLink, image, details) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)');
-		$query->bind_param('sssssssss', $_POST['name'], $_POST['founded'], $_POST['city'], $_POST['stadium'], $_POST['capacity'], $_POST['manager'], $_POST['websiteLink'], $file, $_POST['details']);
+		$query = $mysqli->prepare('INSERT INTO contactList (fname, lname, address, city, zipcode, mnumber, lnumber, relation, photo) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)');
+		$query->bind_param('sssssssss', $_POST['fname'], $_POST['lname'], $_POST['address'], $_POST['city'], $_POST['zipcode'], $_POST['mnumber'], $_POST['lnumber'], $_POST['details'], $file);
 		$query->execute();
 		$mysqli->close();
 	break;
 	// Switch case for the edit team controller
 	case "edit":
 		$id = $_GET['id'];
-		$dataString = $_POST['image'];
+		$dataString = $_POST['photo'];
 
 		if (stripos($dataString, 'base64') !== false){
-			define('UPLOAD_DIR', '../badges/');
+			define('UPLOAD_DIR', '../contactimg/');
 			$img = $dataString;
 			if (preg_match('/png/', $dataString)){
 				$img = str_replace('data:image/png;base64,', '', $img);
@@ -67,17 +67,17 @@ switch($_GET["action"]){
 			$data = base64_decode($img);
 			$success = file_put_contents($file, $data);
 		} else {
-			$file = $_POST['image'];
+			$file = $_POST['photo'];
 		}
-		$query = $mysqli->prepare('UPDATE newteamList SET name = ?, founded = ?, city = ?, stadium = ?, capacity = ?, manager = ?, websiteLink = ?, image = ?, details = ? WHERE id = ?');
-		$query->bind_param('sssssssssi', $_POST['name'], $_POST['founded'], $_POST['city'], $_POST['stadium'], $_POST['capacity'], $_POST['manager'], $_POST['websiteLink'], $file, $_POST['details'], $id);
+		$query = $mysqli->prepare('UPDATE contactList SET fname = ?, lname = ?, address = ?, city = ?, zipcode = ?, mnumber = ?, lnumber = ?, relation = ?, photo = ?, details = ? WHERE id = ?');
+		$query->bind_param('sssssssssi', $_POST['fname'], $_POST['lname'], $_POST['address'], $_POST['city'], $_POST['zipcode'], $_POST['mnumber'], $_POST['lnumber'], $_POST['relation'], $file, $id);
 		$query->execute();
 		$mysqli->close();
 	break;
 
 	case "delete":
 		$id = $_GET['id'];
-		$query = $mysqli->prepare('DELETE FROM newteamList WHERE id = ?');
+		$query = $mysqli->prepare('DELETE FROM contactList WHERE id = ?');
 		$query->bind_param('i', $id);
 		$query->execute();
 		$mysqli->close();
